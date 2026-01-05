94cd280b015ad952c6557745e74fa8762323c9df
Long Bui Thanh <btlong2610@gmail.com>
2026-01-05 09:07:42 +0700

    Update note

--- app/src/main/java/com/example/anew/support/DataTranfer.kt
+++ app/src/main/java/com/example/anew/support/DataTranfer.kt
@@ -3,5 +3,6 @@ package com.example.anew.support
 import com.example.anew.model.User
 
 object DataTranfer {
+    // save user to global when load data
     val userCache = mutableMapOf<String, User>()
 }
